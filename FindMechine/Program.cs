4acdb9211ba23e13163aab36f9bf7e8611b97054
TonyAlvin <2680641586@qq.com>
2022-06-01 09:03:34 +0800

    read from txt

--- FindMechine/Program.cs
+++ FindMechine/Program.cs
@@ -7,16 +7,25 @@ namespace FindMechine
     {
         static void Main(string[] args)
         {
-            string input_str;   // usr input
-            Regex pattern = new Regex("([H,h]ello )world"); // 根据用例与介绍，H不区分大小写，其余不知道
+            string input_line;
             Console.Write("Hi, input your name please:");
             string usr_name = Console.ReadLine();   // 先获取名字
-            while (true)
+            Regex pattern = new Regex("([H,h]ello )world");
+            System.IO.TextReader txt_file = new System.IO.StreamReader("../input.txt");
+            while ((input_line = txt_file.ReadLine()) != null)
             {
-                Console.Write("input a string now:");
-                input_str = Console.ReadLine(); // get input string
-                Console.WriteLine(pattern.Replace(input_str, "$1" + usr_name));
+                Console.WriteLine(pattern.Replace(input_line, "$1" + usr_name));
             }
+            // string input_str;   // usr input
+            // Regex pattern = new Regex("([H,h]ello )world"); // 根据用例与介绍，H不区分大小写，其余不知道
+            // Console.Write("Hi, input your name please:");
+            // string usr_name = Console.ReadLine();   // 先获取名字
+            // while (true)
+            // {
+            //     Console.Write("input a string now:");
+            //     input_str = Console.ReadLine(); // get input string
+            //     Console.WriteLine(pattern.Replace(input_str, "$1" + usr_name));
+            // }
         }
     }
 }
