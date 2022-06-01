d052578e2697d37fe9565a53880b9799a44c8193
TonyAlvin <2680641586@qq.com>
2022-06-01 21:27:48 +0800

    event drive model

--- Program.cs
+++ Program.cs
@@ -8,54 +8,48 @@ namespace findMechine2
     {
         private static ConcurrentQueue<string> outputQueue = new ConcurrentQueue<string>();// 声明输出队列
         private static ConcurrentQueue<string> inputQueue = new ConcurrentQueue<string>();// 输入队列
-
-        static void replace_line()
-        {
-            while (true)
-            {
-                if (inputQueue.TryDequeue(out string newline))
-                {
-                    Console.WriteLine("replacing!");
-                    var result = newline.Replace("Hello world", "Hello legen");
-                    outputQueue.Enqueue(result);
-                }
-            }
-        }
-
-        static void dispay_line()
-        {
-            while (true)
-            {
-                if (outputQueue.TryDequeue(out string newline))
-                {
-                    Console.WriteLine("displaying!");
-                    Console.WriteLine(newline);
-                }
-            }
-        }
-        static void read_line()
-        {
-            string line = "";
-            using (System.IO.StreamReader sr = new System.IO.StreamReader("./input.txt"))
-            {
-                while ((line = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine("inputing line.");
-                    inputQueue.Enqueue(line);
-                    //newLineArrived?.Invoke(line);
-                }
-            }
-        }
-
+        private static Action ReadFinished = null;             // 读取完成的动作
+        private static Action<string> ReadLine = null;         // 读取动作
+        private static Action<string> ProcessedLine = null;     // 操作结束动作
         static void Main(string[] args)
         {
             Console.WriteLine("Begin Process...");
-            // 三个子线程，顺序无关，异步执行
-            Task.Run(() => { read_line(); });   // 开启一个线程去从文件读取数据，放到输入队列里
-            Task.Run(() => { replace_line(); });// 开启一个线程去从输入队列中取数据，处理完成后，放到输出队列里
-            Task.Run(() => { dispay_line(); }); // 开启一个线程从输出队列中取数据，取到后放到显示出来
+
+            ReadFinished += () => { Console.WriteLine("读取完成后，我需要进行处理"); };
+            ReadLine += (input) => { inputQueue.Enqueue(input); };
+            ProcessedLine += (str) => { Console.WriteLine("hi" + str); };
+            ProcessedLine += (str) => { ; };    // 可以添加写文件的程序
+
+            //开启一个线程去读取文件
+            Task.Run(() =>
+            {
+                string line = "";
+                using (System.IO.StreamReader sr = new System.IO.StreamReader("./input.txt"))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        ReadLine?.Invoke(line);
+                    }
+                    ReadFinished?.Invoke();
+                }
+            });
+
+            //开启一个线程去从输入队列中取数据，处理完成后，放到输出队列里
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (inputQueue.TryDequeue(out string newline))
+                    {
+                        var result = newline.Replace("Hello world", "Hello legen");
+                        ProcessedLine?.Invoke(result);
+                    }
+                }
+            });
             Console.WriteLine("Any key to exit...");
             Console.ReadKey(true);              // 等待按键以结束程序，否则主线程结束后，创建的几个线程会被结束。
         }
     }
 }
+
