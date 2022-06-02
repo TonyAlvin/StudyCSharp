c45b7f5d2da773ce6bb33e67f18e01a439cee033
TonyAlvin <2680641586@qq.com>
2022-06-02 14:03:44 +0800

    receiver created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace socket_receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
