3341e073e06dbcc5f0a199a9c098e48c0fa0817c
TonyAlvin <2680641586@qq.com>
2022-06-02 14:01:46 +0800

    sender created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace socket_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
